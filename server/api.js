1d2fe129fec52714749ee8f10bcf9bf586fe5cf9
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2022-01-24 16:49:29 +0000

    Create minionsRouter in minions.js and mount it to apiRouter

--- server/api.js
+++ server/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+//Require a minions router
+const minionsRouter = require('minions.js');
 
+//Mount the minions router
+apiRouter.use('/minions', minionsRouter);
 
 module.exports = apiRouter;
