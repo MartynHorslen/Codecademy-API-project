c2fc50e70223c63b67cc694d20e43e5476ab04ed
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2022-01-24 17:19:02 +0000

    Add route to get all minions

--- server/minions.js
+++ server/minions.js
@@ -10,7 +10,16 @@ const {
     deleteAllFromDatabase,
   } = require('./db.js');
 
-
+//GET /api/minions
+minionsRouter.get('/', (req, res, next) => {
+    const allMinions = getAllFromDatabase('minions');
+    if (allMinions === -1){
+        res.status(404).send();
+    } else {
+        console.log(allMinions);
+        res.status(200).send(allMinions);
+    }
+});
 
 
 module.exports = minionsRouter;
