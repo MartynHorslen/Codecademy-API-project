53cd671cd831eb8a995357373b75fa361f3dd74b
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2022-01-24 16:51:16 +0000

    Require the db functions

--- server/minions.js
+++ server/minions.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const minionsRouter = express.Router();
+const {
+    createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+  } = require('db.js');
 
 
 
