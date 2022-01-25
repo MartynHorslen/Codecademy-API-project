2224d71aa3c408659a4c90b44c9e8ecb464dc07a
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2022-01-25 14:20:58 +0000

    Add GET request for minion work

--- server/work.js
+++ server/work.js
@@ -2,10 +2,24 @@ const express = require('express');
 const req = require('express/lib/request');
 const { type } = require('express/lib/response');
 const workRouter = express.Router();
+const {
+    createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+  } = require('./db.js');
 
 //GET /api/minions/:minionId/work
 workRouter.get('/', (req, res, next) => {
-    console.log(req.body.id);
+    const work = getAllFromDatabase('work');
+    const minionWork = [];
+    minionWork.push(work.find(element => {
+        return req.body.id == element.minionId;
+    }));
+    res.status(200).send(minionWork);
 });
 
 module.exports = workRouter;
