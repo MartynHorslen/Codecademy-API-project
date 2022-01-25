290334c99236dfda96cb88550eae1024482b07de
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2022-01-25 10:01:11 +0000

    Validate and POST new minion

--- server/minions.js
+++ server/minions.js
@@ -11,6 +11,10 @@ const {
     deleteAllFromDatabase,
   } = require('./db.js');
 
+  
+//Param matching for :id
+//minionsRouter.use('id', (req, res, next, id) => {});
+
 //GET /api/minions
 minionsRouter.get('/', (req, res, next) => {
     const allMinions = getAllFromDatabase('minions');
@@ -31,9 +35,28 @@ minionsRouter.get('/:id', (req, res, next) => {
     }
 });
 
-minionsRouter.post('/', (req, res, next) => {
-    
+//POST & PUT minions validation
+const validation = (req, res, next) => {
+    const name = req.body.name;
+    const title = req.body.title;
+    const salary = Number(req.body.salary);
+    const weaknesses = req.body.weaknesses;
+    if (name === '' || title === '' || salary === '' || weaknesses === ''){
+        res.status(400).send();
+    } else {
+        next();
+    }
+}
+
+//POST /api/minions
+minionsRouter.post('/', validation, (req, res, next) => {
+    const createMinion = addToDatabase('minions', req.body);
+    res.status(201).send(createMinion);
 });
 
+//PUT /api/minions
+minionsRouter.put('/:id', validation, (req, res, next) => {
+    console.log('PUT');
+});
 
 module.exports = minionsRouter;
